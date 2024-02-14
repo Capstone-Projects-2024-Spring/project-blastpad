790a96db68f1059996e32f069823abb62e887688
Mustafa Wedee <104322948+MWedee@users.noreply.github.com>
2024-02-13 19:20:13 -0500

    Added flask server to serve the JS and render html

---
+++ blockly/src/serve.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
