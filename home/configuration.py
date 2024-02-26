b61aa28929ef5e7a2625c6e51337119033fb049d
Niaz Baharudeen <tuk05348@temple.edu>
2024-02-25 22:47:39 -0500

    Created empty shells of python files for sphinx.

---
+++ home/configuration.py
@@ -0,0 +1,18 @@
+class Configuration:
+    def connectToNetwork(SSID: str, networkKey: str):
+        """
+        Connect to given network using SSID and network key 
+        """
+    def listNetworks() -> str:
+        """
+        List available networks and return as string
+        """
+        return ''
+    def scan():
+        """
+        Scan for new networks
+        """
+    def disconnectFromNetwork():
+        """
+        Disconnect from current network
+        """
