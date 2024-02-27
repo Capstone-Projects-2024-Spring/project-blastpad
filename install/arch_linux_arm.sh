59c78b72aefbaa24cbafedc5c71ba372b6fc59df
Jacob Snarr <jacobsnarr@gmail.com>
2024-02-26 21:06:57 -0500

    Add quotes to concatenate variable and number

--- install/arch_linux_arm.sh
+++ install/arch_linux_arm.sh
@@ -18,15 +18,15 @@ SDX=$1
   echo w
 ) | sudo fdisk $SDX
 
-mkfs.vfat /dev/$SDX1
+mkfs.vfat "/dev/$SDX1"
 rmdir boot
 mkdir boot
-mount /dev/$SDX1 boot
+mount "/dev/$SDX1" boot
 
-mkfs.ext4 /dev/$SDX2
+mkfs.ext4 "/dev/$SDX2"
 rmdir boot
 mkdir root
-mount /dev/$SDX2 root
+mount "/dev/$SDX2" root
 
 wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-armv7-latest.tar.gz
 bsdtar -xpf ArchLinuxARM-rpi-armv7-latest.tar.gz -C root
