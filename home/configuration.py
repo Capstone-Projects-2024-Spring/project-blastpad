3b0be35840c6105fe010f678bacb44d33b723cfa
Niaz Baharudeen <tuk05348@temple.edu>
2024-02-29 22:58:46 -0500

    Set up branch with written configuration functions.

--- home/configuration.py
+++ home/configuration.py
@@ -1,3 +1,4 @@
+import subprocess
 class Configuration:
     connectionStatus = False
     listOfAvailNetworks = []
@@ -8,6 +9,8 @@ class Configuration:
         """
         Connect to given network using SSID and network key 
         """
+        p = subprocess.run(f'nmcli device wifi connect {SSID} password {securityKey}'.split(), capture_output=True, text=True).stdout
+        Configuration.connectionStatus = True
     def listNetworks() -> str:
         """
         List available networks and return as string
@@ -17,8 +20,11 @@ class Configuration:
         """
         Scan for new networks
         """
+        Configuration.listOfAvailNetworks = subprocess.run('nmcli device wifi list'.split(), capture_output=True, text=True).stdout
         return Configuration.listOfAvailNetworks
     def close():
         """
         Disconnect from current network
         """
+        p = subprocess.run('nmcli d disconnect wlan0'.split(), capture_output=True, text=True).stdout
+        print(p)
