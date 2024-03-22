563add84de8c47e2ac7078a644b3768461cb2143
Mustafa <tug62328@temple.edu>
2024-03-22 17:04:25 -0400

    updated

--- home/homeScreenGui.py
+++ home/homeScreenGui.py
@@ -1,9 +1,22 @@
 import tkinter as tk
-from tkinter import Canvas, Frame, Scrollbar, Label
+from tkinter import Canvas, Frame, Scrollbar, Label, ttk, filedialog
+import customtkinter as ctk 
 import webbrowser
 from PIL import Image, ImageTk
 import os
 from time import strftime
+import ttkbootstrap as tb 
+from ttkbootstrap.toast import ToastNotification
+
+def construct_toast(title, message, alert):
+    # Use the function arguments to customize the toast notification
+    toast = ToastNotification(title=title, message=message, duration=3000,bootstyle="dark", alert=alert)
+    
+    return toast
+
+def show_toast(root, toast):
+    toast.show_toast()
+    # root.configure(bg='#33363e')
 
 # Function that updates time
 def update_time(label):
@@ -98,18 +111,27 @@ def render_new_game_icon(game_list_frame, button_width, button_height):
     pil_img = Image.open('home\\guiImages\\newGameIcon.png')
     pil_img = pil_img.resize((button_width, button_height), Image.LANCZOS)
 
-    # Bind enter and leave events to the game frame to show mous hovering effects
-    new_game_frame.bind("<Enter>", lambda e, widget=new_game_frame: on_enter(e, widget))
-    new_game_frame.bind("<Leave>", lambda e, widget=new_game_frame: on_leave(e, widget))
+    # Create a PhotoImage object from the resized PIL image
+    photo = ImageTk.PhotoImage(pil_img)
 
-#Function where it opens a new web browser tab that shows a new game page
-def open_code_editor_new_game_page(event):
+    # Create a Tkinter button with this image
+    img_button = tk.Button(game_list_frame, image=photo, command=open_code_editor_new_game_page, borderwidth=0, highlightthickness=0)
+    img_button.image = photo  # Keep a reference to the image
+    img_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+    img_button.bind("<Enter>", lambda e, widget=img_button: on_enter(e, widget))
+    img_button.bind("<Leave>", lambda e, widget=img_button: on_leave(e, widget))
+
+
+# Function where it opens a new web browser tab that shows a new game page
+def open_code_editor_new_game_page():
     print("New Game Icon button clicked!")
+    show_toast(root,construct_toast("New Game Editor","New Game Code Editor Launched!",True))
     link = 'http://localhost:5000/?load=filename.json'
     webbrowser.open(link)
 
 
-#Opening an instance of the code editor
+# Opening an instance of the code editor
 def open_code_editor():
     link = 'http://localhost:5000'
     webbrowser.open(link)
@@ -123,19 +145,22 @@ def on_leave(e, widget):
     widget.config(highlightbackground='grey', highlightthickness=1)
 
 
-def display_game_info(game_info_container, game_name):
+def display_game_info(game_info_container, game_name, game_json_path):
     # Clear any existing widgets in the game information container
     for widget in game_info_container.winfo_children():
         widget.destroy()
 
-    # Create a frame for the text information
-    text_info_frame = tk.Frame(game_info_container)
+    # Style update: Game information container background color
+    game_info_container.config(bg='#23252C')
+
+    # Create a frame for the text information with padding and background color
+    text_info_frame = tk.Frame(game_info_container, padx=10, pady=10, bg='#23252C')
     text_info_frame.pack(side=tk.LEFT, fill=tk.X, expand=True)
 
-    # Create labels inside the text information frame
-    tk.Label(text_info_frame, text=game_name, font=("Helvetica", 16), anchor='w').pack(fill='x')
-    tk.Label(text_info_frame, text="Author: You", anchor='w').pack(fill='x')
-    tk.Label(text_info_frame, text="Last Updated: 2/13/2024", anchor='w').pack(fill='x')
+    # Add text into gme info frame
+    ctk.CTkLabel(text_info_frame, text=game_name, font=("Helvetica", 40, "bold"), anchor='w', text_color='#FFFFFF', fg_color='#23252C').pack(fill='x')
+    ctk.CTkLabel(text_info_frame, text="Author: You", font=("Helvetica", 20, "bold") ,anchor='w', text_color='#FFFFFF', fg_color='#23252C').pack(fill='x')
+    ctk.CTkLabel(text_info_frame, text="Last Updated: 2/13/2024", font=("Helvetica", 20, "bold") ,anchor='w', text_color='#FFFFFF', fg_color='#23252C').pack(fill='x')
 
     # Style update for button frame
     button_frame = tk.Frame(game_info_container, bg='#23252C')
@@ -192,168 +217,94 @@ def display_game_info(game_info_container, game_name):
     buttonHeight = 90
 
     # Create buttons with new styling
-    # play_button = create_button(button_frame, play_button_img_path, on_compile_click, buttonWidth, buttonHeight)
     play_button = create_button(button_frame, play_button_img_path, lambda: on_compile_click(game_json_path), buttonWidth, buttonHeight)
     edit_button = create_button(button_frame, edit_button_img_path, open_code_editor, buttonWidth, buttonHeight)
     upload_button = create_button(button_frame, upload_buton_img_path, None, buttonWidth, buttonHeight)
 
 
 def render_game_library(main_container, game_info_container):
-    # Create a container frame with a border
-    container = tk.Frame(main_container, bd=2, relief='groove')
+    # Update the container frame with a background color
+    container = tk.Frame(main_container, bg='#23252C')
     container.pack(side=tk.TOP, fill='both', expand=True, padx=10, pady=10)
-    
-    canvas = tk.Canvas(container, highlightthickness=0)
-    
-    # Create a frame for the scrollbar to ensure it's at the bottom of the container
-    scrollbar_frame = tk.Frame(container)
-    scrollbar_frame.pack(side=tk.BOTTOM, fill='x', expand=False)
-    
-    # Create a scrollbar within the scrollbar frame
-    scrollbar = tk.Scrollbar(scrollbar_frame, orient="horizontal", command=canvas.xview)
-    scrollbar.pack(side=tk.BOTTOM, fill='x', expand=False)
-    
-    # Pack the canvas after the scrollbar frame to fill the remaining space
+
+    canvas = tk.Canvas(container, bg='#23252C', highlightthickness=0)
+
     canvas.pack(side=tk.TOP, fill='both', expand=True)
-    
-    # Configure the canvas
-    canvas.configure(xscrollcommand=scrollbar.set)
-    
-    # Create a frame inside the canvas for the list of games
-    game_list_frame = tk.Frame(canvas)
-    
-    # Add the game list frame to a window in the canvas
+
+    # Style the game list frame with a background color
+    game_list_frame = tk.Frame(canvas, bg='#23252C')
     canvas.create_window((0, 0), window=game_list_frame, anchor='nw')
-    
-    # Define a function to update the canvas's scrollregion
+
     def on_frame_configure(canvas):
         canvas.configure(scrollregion=canvas.bbox('all'))
-    
-    # Bind the Frame's configuration event to update the canvas's scrollregion
+
     game_list_frame.bind('<Configure>', lambda event, canvas=canvas: on_frame_configure(canvas))
-    
+
+    # dimensions for new game icon and games in game library
+    game_lib_button_width = 125
+    game_lib_button_height = 125
+
+    # Invoke new game button function
+    render_new_game_icon(game_list_frame, game_lib_button_width, game_lib_button_height)
+
     # Example list of games to populate the frame
-    games = ["T T T", "Game 2", "Game 3", "Game 4", "Game 5", "Game 6", "Game 7"]
-    
-    #Invoke new gam button function
-    render_new_game_icon(game_list_frame)
+    games = ["Game 1", "Game 2", "Game 3", "Game 4", "Game 5", "Game 6", "Game 7"]
 
     # Add games to the frame
     for game in games:
-        box_width = 125 
-        box_height = 125  
-        
+        box_width = 125
+        box_height = 125
+
         # Create a game frame with the specified size
         game_frame = tk.Frame(game_list_frame, width=box_width, height=box_height, bg='#51535B')
         game_frame.pack_propagate(False)  # Prevents child widgets from altering the frame's size
-        game_frame.pack(side=tk.LEFT, padx=20, pady=20)
-        
+        game_frame.pack(side=tk.LEFT, padx=10, pady=10)
+
         # Create a label with the game name and size, and ensure it's centered
         game_label = tk.Label(game_frame, text=f"{game}\n",
-                            fg='#FFFFFF', bg='#51535B', font=('Helvetica', 20))
+                              fg='#FFFFFF', bg='#51535B', font=('Helvetica', 20))
         game_label.pack(expand=True)  # This will center the text in the frame
-        game_label.bind("<Button-1>", lambda event, name=game: display_game_info(game_info_container, name))
+        game_label.bind("<Button-1>", lambda event, name=game: display_game_info(game_info_container, name, os.path.join(".", "flask", "saved", "Multiplayer Tetris.json")))
 
         # Set the highlightthickness for normal state so that the change is visible on hover
         game_frame.config(highlightbackground='grey', highlightthickness=1)
 
-        # Bind enter and leave events to the game frame to show mous hovering effects
+        # Bind enter and leave events to the game frame to show mouse hovering effects
         game_frame.bind("<Enter>", lambda e, widget=game_frame: on_enter(e, widget))
         game_frame.bind("<Leave>", lambda e, widget=game_frame: on_leave(e, widget))
-    
-    # Update the scrollregion after the UI has been drawn
+
     game_list_frame.update_idletasks()
     on_frame_configure(canvas)
-    
+
     # Bind mousewheel scrolling to the canvas
     def on_mousewheel(event):
-        canvas.xview_scroll(int(-1*(event.delta/120)), "units")
+        canvas.xview_scroll(int(-1 * (event.delta/120)), "units")
     canvas.bind_all("<MouseWheel>", on_mousewheel)
 
 
-# Methods to test if cliking widgets are responsive
-def home_button_clicked_event(event=None):
-    print("Home button clicked!")
-
-def community_hub_clicked_event(event=None):
-    print("Community Hub button clicked!")
-
-def classroom_clicked_event(event=None):
-    print("Classroom button clicked!")
-
-def settings_clicked_event(event=None):
-    print("Settings button clicked!")
-
-def render_top_frame(root):
-    top_frame = tk.Frame(root)
-    top_frame.pack(side=tk.TOP, anchor=tk.NW)
-
-    # Add home button
-    home_canvas = tk.Canvas(top_frame, width=100, height=100)
-    home_canvas.pack(side=tk.LEFT, padx=10, pady=10)
-    home_canvas.create_rectangle(0, 0, 100, 100, fill="#51535B")
-    home_canvas.create_text(50, 50, text="Home", fill="#FFFFFF")
-    home_canvas.bind("<Button-1>", home_button_clicked_event)
-    home_canvas.bind("<Enter>", lambda e, widget=home_canvas: on_enter(e, widget))
-    home_canvas.bind("<Leave>", lambda e, widget=home_canvas: on_leave(e, widget))
-
-
-    # Add Community Hub button
-    community_canvas = tk.Canvas(top_frame, width=100, height=100)
-    community_canvas.pack(side=tk.LEFT, padx=10, pady=10)
-    community_canvas.create_rectangle(0, 0, 100, 100, fill="#51535B")
-    community_canvas.create_text(50, 50, text="CH", fill="#FFFFFF")
-    community_canvas.bind("<Button-1>", community_hub_clicked_event)
-    community_canvas.bind("<Enter>", lambda e, widget=community_canvas: on_enter(e, widget))
-    community_canvas.bind("<Leave>", lambda e, widget=community_canvas: on_leave(e, widget))
-
-
-    # Add Classroom button
-    classroom_canvas = tk.Canvas(top_frame, width=100, height=100)
-    classroom_canvas.pack(side=tk.LEFT, padx=10, pady=10)
-    classroom_canvas.create_rectangle(0, 0, 100, 100, fill="#51535B")
-    classroom_canvas.create_text(50, 50, text="Classroom", fill="#FFFFFF")
-    classroom_canvas.bind("<Button-1>", classroom_clicked_event)
-    classroom_canvas.bind("<Enter>", lambda e, widget=classroom_canvas: on_enter(e, widget))
-    classroom_canvas.bind("<Leave>", lambda e, widget=classroom_canvas: on_leave(e, widget))
-
-
-    # Add Settings Button
-    settings_canvas = tk.Canvas(top_frame, width=100, height=100)
-    settings_canvas.pack(side=tk.LEFT, padx=10, pady=10)
-    settings_canvas.create_rectangle(0, 0, 100, 100, fill="#51535B")
-    settings_canvas.create_text(50, 50, text="Settings", fill="#FFFFFF")
-    settings_canvas.bind("<Button-1>", settings_clicked_event)
-    settings_canvas.bind("<Enter>", lambda e, widget=settings_canvas: on_enter(e, widget))
-    settings_canvas.bind("<Leave>", lambda e, widget=settings_canvas: on_leave(e, widget))
-
-
 def initial_render(root):
-    render_top_frame(root)
+    top_container = tk.Frame(root, bg='#424347')
+    top_container.pack(side=tk.TOP, fill='both', expand=True, padx=4, pady=4)
+    render_top_frame(top_container)
     
     # Main container frame for both the games list and game information
-    main_container = tk.Frame(root)
-    main_container.pack(side=tk.TOP, fill='both', expand=True)
+    main_container = ctk.CTkFrame(master=root, corner_radius=20, fg_color='#23252C')
+    main_container.pack(side=tk.TOP, fill='both', expand=True, padx=20, pady=10)
+
 
     # Create the game information container and pack it at the bottom of the main container
-    game_info_container = tk.Frame(main_container, bd=2, relief='groove')
-    game_info_container.pack(side=tk.BOTTOM, fill='x', padx=10, pady=10)
+    game_info_container = tk.Frame(main_container, bd=0, relief='groove')
+    game_info_container.pack(side=tk.BOTTOM, fill='x', padx=20, pady=20)
     
     render_game_library(main_container, game_info_container)
 
-def display_home_screen():
-    root = tk.Tk()
-    root.geometry("800x450")
-    initial_render(root)
-    root.mainloop()
 
-display_home_screen()
+root = tk.Tk()
+root.geometry("800x450")
+root.configure(bg='#33363e')
+initial_render(root)
+root.mainloop()
 
-
-
-
-
-
-
+# display_home_screen()
 
 
