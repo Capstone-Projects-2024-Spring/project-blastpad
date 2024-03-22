3ec76d0b3255ee6b0c49c8c400a520d8358b7555
Niaz Baharudeen <tuk05348@temple.edu>
2024-03-22 16:47:55 -0400

    Basic template for class based tkinter GUI

---
+++ home/guiClassBased.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+class Navbar(tk.Frame): ...
+class Toolbar(tk.Frame): ...
+class Statusbar(tk.Frame): ...
+class Main(tk.Frame): ...
+
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.statusbar = Statusbar(self, ...)
+        self.toolbar = Toolbar(self, ...)
+        self.navbar = Navbar(self, ...)
+        self.main = Main(self, ...)
+
+        self.statusbar.pack(side="bottom", fill="x")
+        self.toolbar.pack(side="top", fill="x")
+        self.navbar.pack(side="left", fill="y")
+        self.main.pack(side="right", fill="both", expand=True)
