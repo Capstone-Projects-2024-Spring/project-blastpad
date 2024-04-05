38c8aaaaed95a2ef43b1edf1962b33db4db374f4
Neil <neilsemail@me.com>
2024-04-05 02:01:39 -0400

    smuggle build in

--- install/test-stage/00-kiosk/files/firstrun.sh
+++ install/test-stage/00-kiosk/files/firstrun.sh
@@ -6,6 +6,13 @@ echo
 echo ">> Enabling Read-Only Overlay File System"
 sudo raspi-config nonint enable_overlayfs
 sudo raspi-config nonint enable_bootro
+
+echo
+echo ">> Unpacking Built Blastpad Package"
+tar -xf blastpad-build.tar.gz
+
+echo
+echo ">> Enabling Auto-Login (?)"
 sudo raspi-config nonint do_boot_behaviour B2
 
 echo
