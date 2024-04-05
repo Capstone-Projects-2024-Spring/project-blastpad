bcd7800291237bcce267f8013306e43d89dab2f1
Neil <neilsemail@me.com>
2024-04-05 02:36:34 -0400

    ok no pillow image package thing

--- install/test-stage/01-python-shit/01-run.sh
+++ install/test-stage/01-python-shit/01-run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-    pip install flask-cors --break-system-packages
-    pip install pillow --break-system-packages
-    pip install tk --break-system-packages
-    pip install customtkinter --break-system-packages
-    pip install pygame --break-system-packages
-    pip install pgzero --break-system-packages
-    pip install flask --break-system-packages
-    pip install pillow --break-system-packages
-    pip install pil.imagetk --break-system-packages
+    pip install flask-cors --break-system-packages --root-user-action=ignore
+    pip install pillow --break-system-packages --root-user-action=ignore
+    pip install tk --break-system-packages --root-user-action=ignore
+    pip install customtkinter --break-system-packages --root-user-action=ignore
+    pip install pygame --break-system-packages --root-user-action=ignore
+    pip install pgzero --break-system-packages --root-user-action=ignore
+    pip install flask --break-system-packages --root-user-action=ignore
+    pip install pillow --break-system-packages --root-user-action=ignore
 EOF
