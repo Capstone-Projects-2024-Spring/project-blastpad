79a0dfc3f3cab0efef637c34251ace0e5102a6e9
Neil <neilsemail@me.com>
2024-04-06 10:41:54 -0400

    add testing stuff to kisok script

--- install/test-stage/00-kiosk/files/kiosk.sh
+++ install/test-stage/00-kiosk/files/kiosk.sh
@@ -6,14 +6,17 @@ URL=$(head -n 1 /boot/kiosk.url)
 # never blank the screen
 xset s off -dpms
 
-# rotate to portrait mounted TV
-xrandr --output HDMI-1 --rotate left # i don't think this actually works
+xrandr --output HDMI-1 --mode 800x400 # set resolution to something blastpad-like
+# 800x400
 
 # show a splash before browser kicks in
 feh --bg-scale splash.jpg
 
-cd project-blastpad
-python home/gui.py
+
+x-terminal-emulator -e "bash -c 'echo $PATH; read'"
+
+# cd project-blastpad
+# python home/gui.py
 
 # start the cec-client & browser
 # (cec-client | cec2kbd) & browser --fullscreen "${URL:='https://deltazero.cz'}"
