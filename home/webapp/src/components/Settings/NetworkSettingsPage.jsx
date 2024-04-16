87f4b51327b7f367de9b3ba3ca8b6eb824f15854
Neil <neilsemail@me.com>
2024-04-16 18:21:36 -0400

    make fetch relative

--- home/webapp/src/components/Settings/NetworkSettingsPage.jsx
+++ home/webapp/src/components/Settings/NetworkSettingsPage.jsx
@@ -7,7 +7,7 @@ export default function NetworkSettingsPage() {
 
   const fetchNetworks = async () => {
     try {
-      const response = await fetch("http://localhost:8000/get_wifi_networks");
+      const response = await fetch("/get_wifi_networks");
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
@@ -25,7 +25,7 @@ export default function NetworkSettingsPage() {
 
   const handleDisconnectButtonClick = async () => {
     try {
-      const response = await fetch("http://localhost:8000/disconnect_wifi", {
+      const response = await fetch("/disconnect_wifi", {
         method: "POST",
       });
       if (!response.ok) {
@@ -43,7 +43,7 @@ export default function NetworkSettingsPage() {
 
   const connectNetwork = async (ssid, password) => {
     try {
-      const response = await fetch('http://localhost:8000/connect_to_wifi', {
+      const response = await fetch('/connect_to_wifi', {
         method: 'POST',
         body: JSON.stringify({ ssid, password }),
         headers: {
